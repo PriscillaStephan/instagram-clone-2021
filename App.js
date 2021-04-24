6e77c9e89d167a4e1d52df09a3192b8f43783688
unknown <pressistephan@gmail.com>
2021-04-24 21:39:48 +0300

    Handling navigation, adding bottom tab navigator

--- App.js
+++ App.js
@@ -8,7 +8,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
 import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
+
 
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
@@ -76,6 +79,7 @@ export class App extends Component {
           <Stack.Navigator initialRouteName="Landing">
             <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}} /> 
             <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" componen={LoginScreen} />
           </Stack.Navigator>
         </NavigationContainer>
     
@@ -84,8 +88,12 @@ export class App extends Component {
 
     return(
       <Provider store={store}>
-                <MainScreen/>
-
+         <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+              <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false}} /> 
+              <Stack.Screen name="Add" component={AddScreen}  /> 
+          </Stack.Navigator>
+         </NavigationContainer>
       </Provider>
     )
    
--- components/Main.js
+++ components/Main.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialComunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import { connect } from 'react-redux'
 import { bindActionCreators }  from 'redux'
 import { fetchUser } from "../redux/actions/index"
+import FeedScreen from './main/Feed'
+import ProfileScreen from './main/Profile'
+
 import * as firebase from 'firebase'
 import '@firebase/firestore';
+import { ColorPropType } from 'react-native';
+
+const Tab = createBottomTabNavigator();
+const EmptyScreen = () => {
+    return(null)
+}
 
 export class Main extends Component {
     componentDidMount(){
        this.props.fetchUser();
     }
-    render() {
-        const { currentUser }  = this.props;
-        console.log()
-        if(currentUser==undefined){
-            return(
-                <View></View>
-            )
-        }
+    render() {     
         return (
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text> {currentUser.name} is Logged In</Text>
-      </View>
+            <Tab.Navigator> 
+                <Tab.Screen name="Feed" component={FeedScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialComunityIcons name="home" color={color} size={26}/>                        
+                        ),
+                }} />
+                <Tab.Screen name="AddContainer" component={EmptyScreen}
+                    //changing the tab click behaviour
+                    listeners={({ navigation }) => ({
+                        tabPress: event => {
+                            event.preventDefault();
+                            navigation.navigate("Add")
+                        }
+                    })}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialComunityIcons name="plus-box" color={color} size={26}/>                        
+                        ),
+                }} />
+                 <Tab.Screen name="Profile" component={ProfileScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialComunityIcons name="account-circle" color={color} size={26}/>                        
+                        ),
+                }} />
+            </Tab.Navigator>
         )
     }
 }
